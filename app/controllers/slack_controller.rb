de47cfbcee69791bebb342e5d44952f7cee4a25d
Jakub <jakub.gorecki19@gmail.com>
2022-02-05 17:51:38 +0100

    Implement booking via slack

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,7 +1,16 @@
 class SlackController < ApplicationController
-  skip_before_action :set_current_user
+  skip_before_action :set_current_user, :verify_authenticity_token
 
   def book
-    puts params
+    user = User.find_by(slack_id: params["user_id"])
+    user ||= User.create(slack_id: params["user_id"], name: params["user_name"])
+    
+    if BookingService.perform(user, params["text"].to_i)
+      json = { text: "You've booked the place."}
+    else
+      json = { text: "Something went wrong."}
+    end
+
+    render json: json
   end
 end
